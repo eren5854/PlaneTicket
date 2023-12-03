5080295e7a696718382dd4cee21437a22edb353c
eren5854 <ihsanerendelibas@hotmail.com>
2023-12-04 00:23:25 +0300

    .
    4.12.2023

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlaneTicketMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Seat.cs
@@ -0,0 +1,8 @@
+﻿namespace PlaneTicketMVC.Models;
+
+public class Seat
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+}
